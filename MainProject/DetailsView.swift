9161b0fe7cb304cbe3d5d12c225f0b99116dc5b4
Vignesh Kumar <vigkumar@WKMZTC401E41.global.publicisgroupe.net>
2022-09-02 11:58:06 +0530

    added details view in main project

---
+++ MainProject/DetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailsView.swift
+//  MainProject
+//
+//  Created by Vignesh Kumar on 02/09/22.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailsView()
+    }
+}
