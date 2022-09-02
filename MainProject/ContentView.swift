47699febc6e8da5b2cf0c4de6f402f0605c99eff
Vignesh Kumar <vigkumar@WKMZTC401E41.global.publicisgroupe.net>
2022-09-02 11:40:38 +0530

    First commit

---
+++ MainProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MainProject
+//
+//  Created by Vignesh Kumar on 02/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MainProject/MainProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainProjectApp.swift
+//  MainProject
+//
+//  Created by Vignesh Kumar on 02/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MainProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
